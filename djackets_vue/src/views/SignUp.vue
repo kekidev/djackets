9f83bed888ebda44bcd2adc11991ba913b2f8f5d
keki <baejaehwan1125@gmail.com>
2021-07-03 19:50:46 +0900

    first init

---
+++ djackets_vue/src/views/SignUp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-sign-up"></div>
+</template>
+
+<script>
+import axios from "axios";
+import { toast } from "bulma-toast";
+
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      username: "",
+      password: "",
+      password2: "",
+      errors: [],
+    };
+  },
+};
+</script>
